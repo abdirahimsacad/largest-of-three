30f3514ac73daf4a39ee8e119c4104e247245a12
abdirahimsacad <abdirahiimsacad@gmail.com>
2023-07-26 14:43:17 -0700

    upadate

--- challenge.js
+++ challenge.js
@@ -1,2 +1,15 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+const x = 5;
+const y = 10;
+const w = 15; 
+		
+
+if(x > y && x > w){
+	console,console.log(`${x} Is the Largest`);
+}else if(y > x && y > w){
+	console,console.log(`${y} Is the Largest`);
+}else{
+	console,console.log(`${w} Is the Largest`);
+		}
